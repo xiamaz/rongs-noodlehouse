a211ed00268ae8815270040cd321f56e4f5992e3
Max Zhao <max.zhao@arsbrevis.de>
2022-05-15 19:14:40 +0200

    Add height and width to image

--- components/Title.vue
+++ components/Title.vue
@@ -1,7 +1,7 @@
 <template lang="">
 	<NuxtLink :to="localePath('/')">
 	<div class="flex justify-center items-center">
-		<img src="~/assets/logo-sym.svg" class="w-24 ml-2 mr-4 md:mr-7" alt="" srcset="">
+		<img src="~/assets/logo-sym.svg" width="96" height="96" class="w-24 ml-2 mr-4 md:mr-7" alt="" srcset="">
 		<span class="text-4xl md:text-5xl text-gray-700 font-black">
 			Rong's Noodlehouse
 		</span>
