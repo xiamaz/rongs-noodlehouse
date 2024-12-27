6270759f4edcb7b9008d75b25ca1a18990f735aa
Max Zhao <max.zhao@arsbrevis.de>
2024-12-27 11:55:39 +0100

    .

--- pages/impressum.vue
+++ pages/impressum.vue
@@ -1,8 +1,7 @@
 <template>
 	<div class="flex flex-col items-center text-md">
 		<div class="font-black text-lg">{{$t("imprint")}}</div>
-		<div>Max Xiaohang Zhao</div>
-		<div>{{$t("owner")}}: Yan Rong</div>
+		<div>Inh. Yan Rong</div>
 		<div>Rong's Noodlehouse</div>
 		<div>Leibnizstraße 101</div>
 		<div>D-10625 Berlin</div>
